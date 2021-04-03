b1b8c89957dceba07467f9b7f11e0239819e6d98
Joshua Ordaz <joshuaordaz@comcast.net>
2021-04-02 23:30:03 -0700

    deploying

--- server.js
+++ server.js
@@ -12,10 +12,15 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/fitness-tracker", {
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://localhost/fitness-tracker',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  }
+);
 
 app.use(require("./routes/html-routes.js"));
 app.use(require("./routes/api-routes.js"));
